7f052d0cc4085cd2fd23a8e0791e412ee0376aca
Shivam Yadav <yadav.shivam8130@gmail.com>
2023-01-16 00:03:26 +0530

    commiting all files to git repo

---
+++ src/main/java/com/cloudGateway/HotelRatingsCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cloudGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRatingsCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRatingsCloudGatewayApplication.class, args);
+	}
+
+}
